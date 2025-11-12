2474955a4c54f8def1903a3352e18a68235ded5e
rashmi2923 <rashmirashmi3170@gmail.com>
2025-11-12 15:41:21 +0530

    Create name,age,roll number.py

---
+++ name,age,roll number.py
@@ -0,0 +1,11 @@
+
+def moooo(x,y):
+ name=(input("enter your name:"))
+ age=int(input("enter your age:"))
+ roll_num=int(input("enter your roll_num:"))
+ addition=age+roll_num
+ print("the sum is:",addition)
+ multi=x*y
+ print("the multi is:",multi)
+ return name,age,roll_num
+moooo(5,10)
